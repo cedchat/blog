a698a9b4264820b96a4c6a41df2df4def35a158b
cedchat <cedric.chatard@free.fr>
2015-07-02 11:50:14 +0200

    add favicon.ico

--- app.js
+++ app.js
@@ -18,6 +18,9 @@ var options = {
 	cert: fs.readFileSync('ssl/cert.pem')};
 
 var app = express();
+
+app.use(favicon(__dirname + '/public/files/favicon.ico'));
+
 https.createServer(options, app).listen(443);
 
 // view engine setup
@@ -31,8 +34,6 @@ var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 
 app.use(require('stylus').middleware({ src: __dirname + '/public' }));
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
